8b34bb219871d51af14876f45bdb4159cab864d7
Boonwarat <Boonwarat074@gmail.com>
2025-10-08 19:27:36 +0700

    refactor : optimise for faster?

--- AvlTree.py
+++ AvlTree.py
@@ -8,15 +8,34 @@ class AVLTree:
         self.root = self._insert(self.root, room)
 
     def _insert(self, node, room):
-        if not node:
+        if node is None:
             return Node(Room(room[0], room[1]))
-        elif room[0] >= node.data.num:
-            node.right = self._insert(node.right,room)
-        elif room[0] < node.data.num:
-            node.left = self._insert(node.left,room)
 
-        
-        node = self.balance(node)
+        key = room[0]
+        if key < node.data.num:
+            node.left = self._insert(node.left, room)
+        else:
+            node.right = self._insert(node.right, room)
+
+        # Update height once
+        lh = node.left.height if node.left else -1
+        rh = node.right.height if node.right else -1
+        node.height = 1 + (lh if lh > rh else rh)
+
+        balance = rh - lh
+
+        # Right heavy
+        if balance == 2:
+            if node.right and (node.right.right is None or node.right.balanceFactor() < 0):
+                node.right = node.right.rotateRight()
+            return node.rotateLeft()
+
+        # Left heavy
+        if balance == -2:
+            if node.left and (node.left.left is None or node.left.balanceFactor() > 0):
+                node.left = node.left.rotateLeft()
+            return node.rotateRight()
+
         return node
     
     def balance(self, node):
@@ -38,72 +57,83 @@ class AVLTree:
         node.setHeight()
         return node
 
-    def move_room(self,method,n=0):
-        return self._move_room(self.root,method,n)
-    
-    def _move_room(self,node,method,n):
-        if not node:
+    def move_room(self, method, n=0):
+        if not self.root:
             return
-        
-        node.data.num = process_room_number(method, n, node.data.num)
-
-        node.left = self._move_room(node.left,method,n)
-        node.right = self._move_room(node.right,method,n)
-
-        return node 
+        stack = [self.root]
+        while stack:
+            node = stack.pop()
+            node.data.num = process_room_number(method, n, node.data.num)
+            if node.left:
+                stack.append(node.left)
+            if node.right:
+                stack.append(node.right)
+        return self.root
     
     def removeRoom(self, roomNumber):
         self.root, removed = self._remove(self.root, roomNumber)
         return removed
 
     def _remove(self, node, roomNumber):
-        if not node:
-            return node, None  
+        if node is None:
+            return None, None
 
         if roomNumber < node.data.num:
             node.left, removed = self._remove(node.left, roomNumber)
         elif roomNumber > node.data.num:
             node.right, removed = self._remove(node.right, roomNumber)
         else:
-            removed = node.data  
-
-            if not node.left:
+            removed = node.data
+            if node.left is None:
                 return node.right, removed
-            elif not node.right:
+            if node.right is None:
                 return node.left, removed
 
+            # Find smallest in right subtree
             successor = node.right
             while successor.left:
                 successor = successor.left
-
             node.data = successor.data
             node.right, _ = self._remove(node.right, successor.data.num)
 
+        # Rebalance after removal
+        if node:
+            lh = node.left.height if node.left else -1
+            rh = node.right.height if node.right else -1
+            node.height = 1 + (lh if lh > rh else rh)
+            balance = rh - lh
+            if balance == 2:
+                if node.right and node.right.balanceFactor() < 0:
+                    node.right = node.right.rotateRight()
+                node = node.rotateLeft()
+            elif balance == -2:
+                if node.left and node.left.balanceFactor() > 0:
+                    node.left = node.left.rotateLeft()
+                node = node.rotateRight()
         return node, removed
 
 
     def get_all(self):
-        return self._get_all(self.root)
+        result = []
+        stack = []
+        node = self.root
+        while stack or node:
+            if node:
+                stack.append(node)
+                node = node.left
+            else:
+                node = stack.pop()
+                result.append(node.data)
+                node = node.right
+        return result
     
-    def _get_all(self,node):
-        if not node:
-            return []
-        return self._get_all(node.left) + [node.data] + self._get_all(node.right)
-    
-    def search(self,room):
-        return self._search(self.root,room)
-    
-    def _search(self,node,room):
-        if not node:
-            return None
-        
-        if node.data.num == room:
-            return node.data
-        elif node.data.num > room:
-            return self._search(node.left,room)
-        elif node.data.num < room:
-            return self._search(node.right,room)
-
+    def search(self, room):
+        node = self.root
+        while node:
+            if node.data.num == room:
+                return node.data
+            node = node.left if room < node.data.num else node.right
+        return None
 
 
 class Node:
